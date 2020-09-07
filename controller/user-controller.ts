85e393c92311a311bf0f0f524782a12777ce6ddb
VitorMac10 <vitormac3@hotmail.com>
2020-09-06 21:49:49 -0300

    Invertido ordem das rotas

--- controller/user-controller.ts
+++ controller/user-controller.ts
@@ -10,6 +10,27 @@ export declare interface UserRequest extends Request {
 
 }
 
+route.get('/users', (req, res) => {
+    const { limit, offset } = req.query;
+    if (!limit || !offset) {
+        return res.status(400).send({
+            mensagem: 'Parâmetros de consulta não informados'
+        });
+    }
+
+    Service.getUsers(Number(limit), Number(offset)).then(users => {
+        res.status(200).send(users);
+    }).catch(() => {
+        res.status(400).send({
+            mensagem: 'Não foi possível localizar os usuários'
+        });
+    });
+});
+
+route.get('/users/filter', (req, res) => {
+
+});
+
 route.get('/:user_id', (req, res) => {
     Service.getUser(req.params['user_id']).then(user => {
         res.status(200).send(user);
@@ -57,27 +78,6 @@ route.delete('/:user_id', (req, res) => {
             mensagem: 'Erro ao deletar usuário'
         });
     });
-});
-
-route.get('/users', (req, res) => {
-    const { limit, offset } = req.query;
-    if (!limit || !offset) {
-        return res.status(400).send({
-            mensagem: 'Parâmetros de consulta não informados'
-        });
-    }
-
-    Service.getUsers(Number(limit), Number(offset)).then(users => {
-        res.status(200).send(users);
-    }).catch(() => {
-        res.status(400).send({
-            mensagem: 'Não foi possível localizar os usuários'
-        });
-    });
-});
-
-route.get('/users/filter', (req, res) => {
-
 });
 
 export default route;
